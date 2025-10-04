6e38052aec02ad45e874fab4ad72589ed6664380
clankpan <clankpan26cm@gmail.com>
2025-10-04 13:03:02 +0900

    WIP

--- src/ar.rs
+++ src/ar.rs
@@ -1,10 +1,12 @@
 use num_traits::{One, Zero};
-use std::cell::RefCell;
+use std::{cell::RefCell, collections::HashMap, ops::Add};
+
+type M<T> = HashMap<usize, T>;
 
 #[derive(Debug)]
 pub struct Arena<T> {
     pub(crate) wit: RefCell<Vec<T>>,
-    pub(crate) exp: RefCell<Vec<(Vec<(usize, T)>, Vec<(usize, T)>, Vec<(usize, T)>, usize)>>,
+    pub(crate) exp: RefCell<Vec<(M<T>, M<T>, M<T>, usize)>>,
 }
 
 impl<T: One> Default for Arena<T> {
@@ -16,7 +18,7 @@ impl<T: One> Default for Arena<T> {
     }
 }
 
-impl<T: One + Zero> Arena<T> {
+impl<T: Copy + One + Zero> Arena<T> {
     #[inline]
     pub fn disable(&self) {
         self.wit.borrow_mut()[0] = T::zero(); // 定数項をゼロに
@@ -37,6 +39,18 @@ impl<T: One + Zero> Arena<T> {
 
     #[inline]
     pub fn exp(&self, a: Vec<(usize, T)>, b: Vec<(usize, T)>, c: Vec<(usize, T)>, idx: usize) {
+        let (a, b, c) = (sum_by_key(a), sum_by_key(b), sum_by_key(c));
         self.exp.borrow_mut().push((a, b, c, idx));
     }
+}
+
+fn sum_by_key<T>(a: Vec<(usize, T)>) -> HashMap<usize, T>
+where
+    T: Add<Output = T> + Copy,
+{
+    let mut map = HashMap::new();
+    for (k, v) in a {
+        map.entry(k).and_modify(|acc| *acc = *acc + v).or_insert(v);
+    }
+    map
 }
